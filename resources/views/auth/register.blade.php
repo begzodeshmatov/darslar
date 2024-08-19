0457363df39624598458df1ebd38ab8b1d18c46a
begzod_eshmatov <eshmatovbegzod16@gmail.com>
2024-08-19 17:28:29 +0500

    login and register finish

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,145 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <title>Pages / Register - NiceAdmin Bootstrap Template</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+    <!-- Favicons -->
+    <link href="admin/assets/img/favicon.png" rel="icon">
+    <link href="admin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+    <!-- Vendor CSS Files -->
+    <link href="admin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="admin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="admin/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="admin/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="admin/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="admin/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="admin/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="admin/assets/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Updated: Apr 20 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    <main>
+        <div class="container">
+
+            <section
+                class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+                            <div class="d-flex justify-content-center py-4">
+                                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                                    <img src="assets/img/logo.png" alt="">
+                                    <span class="d-none d-lg-block">Kutubxona</span>
+                                </a>
+                            </div><!-- End Logo -->
+
+                            <div class="card mb-3">
+
+                                <div class="card-body">
+
+                                    <div class="pt-4 pb-2">
+                                        <h5 class="card-title text-center pb-0 fs-4">Create an Account</h5>
+                                    </div>
+
+                                    <form class="row g-3 needs-validation" action="/register" method="POST" novalidate>
+                                        @csrf
+                                        <div class="col-12">
+                                            <label for="yourName" class="form-label">Your Name</label>
+                                            <input type="text" name="name" class="form-control" id="yourName"
+                                                required>
+                                            <div class="invalid-feedback">Please, enter your name!</div>
+                                        </div>
+
+                                        <div class="col-12">
+                                            <label for="yourEmail" class="form-label">Your Email</label>
+                                            <input type="email" name="email" class="form-control" id="yourEmail"
+                                                required>
+                                            <div class="invalid-feedback">Please enter a valid Email adddress!</div>
+                                        </div>
+
+                                        <div class="col-12">
+                                            <label for="yourUsername" class="form-label">Password</label>
+                                            <div class="input-group has-validation">
+                                                <input type="password" name="password" class="form-control"
+                                                    id="yourUsername" required>
+                                                <div class="invalid-feedback">Please choose a username.</div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12">
+                                            <label for="yourPassword" class="form-label">Confirm Password</label>
+                                            <input type="password" name="password_confirmation" class="form-control"
+                                                id="yourPassword" required>
+                                            <div class="invalid-feedback">Please enter your password!</div>
+                                        </div>
+
+
+                                        <div class="col-12">
+                                            <button class="btn btn-primary w-100" type="submit">Create Account</button>
+                                        </div>
+                                    </form>
+
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <div class="credits">
+                                <!-- All the links in the footer should remain intact. -->
+                                <!-- You can delete the links only if you purchased the pro version. -->
+                                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+                                Designed by <a href="https://t.me/Begzod_Eshmatov/">Strong team</a>
                             </div>
+
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
+
+            </section>
+
         </div>
-    </div>
-</div>
-@endsection
+    </main><!-- End #main -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="admin/assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="admin/assets/vendor/chart.js/chart.umd.js"></script>
+    <script src="admin/assets/vendor/echarts/echarts.min.js"></script>
+    <script src="admin/assets/vendor/quill/quill.js"></script>
+    <script src="admin/assets/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="admin/assets/vendor/tinymce/tinymce.min.js"></script>
+    <script src="admin/assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="admin/assets/js/main.js"></script>
+
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
     <link rel="stylesheet" href="template/css/aos.css">
     <link rel="stylesheet" href="template/css/style.css">
 
-    <title>Learner Free Bootstrap Template by Untree.co</title>
+    <title>Kutubxona</title>
 </head>
 
 <body>
@@ -64,11 +64,11 @@
                     </div>
 
                     <div class="col-6 col-lg-3 text-right">
-                        <a href="login.html" class="small mr-3">
+                        <a href="/login" class="small mr-3">
                             <span class="icon-lock"></span>
                             Log In
                         </a>
-                        <a href="register.html" class="small">
+                        <a href="/register" class="small">
                             <span class="icon-person"></span>
                             Register
                         </a>
